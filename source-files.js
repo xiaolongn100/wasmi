0bfad523dd44deea12311eb92de9107b712dd041
Travis CI User <travis@example.org>
2019-01-29 12:50:26 +0000

    Update documentation

---
+++ source-files.js
@@ -0,0 +1,6 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['byteorder'] = {"name":"","dirs":[],"files":["io.rs","lib.rs"]};
+sourcesIndex['instantiate'] = {"name":"","dirs":[],"files":["instantiate.rs"]};
+sourcesIndex['memory_units'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['parity_wasm'] = {"name":"","dirs":[{"name":"builder","dirs":[],"files":["code.rs","data.rs","export.rs","global.rs","import.rs","invoke.rs","memory.rs","misc.rs","mod.rs","module.rs","table.rs"]},{"name":"elements","dirs":[],"files":["export_entry.rs","func.rs","global_entry.rs","import_entry.rs","index_map.rs","mod.rs","module.rs","name_section.rs","ops.rs","primitives.rs","reloc_section.rs","section.rs","segment.rs","types.rs"]}],"files":["io.rs","lib.rs"]};
+sourcesIndex['wasmi'] = {"name":"","dirs":[{"name":"common","dirs":[],"files":["mod.rs","stack.rs"]},{"name":"validation","dirs":[],"files":["context.rs","func.rs","mod.rs","util.rs"]}],"files":["func.rs","global.rs","host.rs","imports.rs","isa.rs","lib.rs","memory.rs","module.rs","nan_preserving_float.rs","runner.rs","table.rs","types.rs","value.rs"]};
